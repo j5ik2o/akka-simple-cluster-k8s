92d3e2598924ee79cf414af076a34f70decbb265
Junichi Kato <j5ik2o@gmail.com>
2019-05-22 12:36:19 +0900

    add support for k8s namespace.

--- minikube-deploy.sh
+++ minikube-deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+minikube delete
+rm -fr ~/.minikube
+
 minikube start --vm-driver hyperkit --cpus 6 --memory 4096 --disk-size 60g
 eval $(minikube docker-env)
 sbt docker:publishLocal
-kubectl create -f k8s/simple-akka-cluster-rbac.yml
-kubectl create -f k8s/simple-akka-cluster-deployment.yml
-kubectl create -f k8s/simple-akka-cluster-service.yml
+
+kubectl create namespace akka-simple-cluster-ns
+kubectl create serviceaccount akka-simple-cluster-ns
+
+kubectl create -f k8s/simple-akka-cluster-rbac.yml --namespace akka-simple-cluster-ns
+kubectl create -f k8s/simple-akka-cluster-deployment.yml --namespace akka-simple-cluster-ns
+kubectl create -f k8s/simple-akka-cluster-service.yml --namespace akka-simple-cluster-ns
