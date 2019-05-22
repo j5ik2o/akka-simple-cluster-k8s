9b8260175444d553eded4fec3e82245c13407f7e
Junichi Kato <j5ik2o@gmail.com>
2019-05-22 12:17:41 +0900

    refactor

--- minikube-deploy.sh
+++ minikube-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-minikube start
+minikube start --vm-driver hyperkit --cpus 6 --memory 4096 --disk-size 60g
 eval $(minikube docker-env)
 sbt docker:publishLocal
 # create serviceAccount and role
